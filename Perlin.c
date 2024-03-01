dd398a4c77588896f81347f02ad9022a3bb9cba4
Chuks888 <hugotarczynski@gmail.com>
2024-03-01 16:02:39 +0100

    working on vector structure

--- Perlin.c
+++ Perlin.c
@@ -4,6 +4,22 @@
 #include <math.h>
 #include <string.h>
 
+struct Corners
+{
+    struct vector *TopRight;
+    struct vector *TopLeft;
+    struct vector *BottomRight;
+    struct vector *BottomLeft;
+};
+
+struct vector
+{
+    float x1;
+    float y1;
+
+    int permutation_value;
+};  
+
 void swap(int *table, int i, int j)
 {
     int temp = table[i];
@@ -24,9 +40,9 @@ int* Permutation_maker() // function for creating permutation array
 
     // shuffling of array
     int random;
-    for(int i = 0; i < 256; i++)
+    for(int i = 255; i > 0  ; i--)
     {
-        random = rand()%256;
+        random = rand()%i;
         swap(Permutation, i, random);
     }
 
@@ -41,7 +57,14 @@ int* Permutation_maker() // function for creating permutation array
 
 float TDPerlin(float x, float y)
 {
+    int Base_x = floor(x);
+    int Base_y = floor(y);
 
+    int Point_x = x - Base_x;
+    int Point_y = y - Base_y;
+
+    Base_x = Base_x & 256;
+    Base_y = Base_y & 256;
 }
 
 int main()
